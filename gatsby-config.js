9031735edfafdae7125d661e544d319842d79c75
Monica Powell <mmp2181@columbia.edu>
2018-06-07 21:31:27 -0400

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/hello-react',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
