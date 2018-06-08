e2b5e0cff2e46d0c40f10d83181deb3256d2c4c0
Monica Powell <mmp2181@columbia.edu>
2018-06-07 22:50:20 -0400

    added footer to bottom of each blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 
 import Bio from '../components/Bio'
+import Footer from '../components/Footer'
 import { rhythm, scale } from '../utils/typography'
 
 class BlogPostTemplate extends React.Component {
@@ -59,6 +60,7 @@ class BlogPostTemplate extends React.Component {
             </li>
           )}
         </ul>
+        <Footer />
       </div>
     )
   }
