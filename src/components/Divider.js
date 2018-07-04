96915667d783aa26198fc5d735e0595d5fc02828
Monica Powell <mmp2181@columbia.edu>
2018-07-04 15:09:19 -0400

    removed unnecessary commits

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,22 +1,14 @@
 import React from 'react'
 
-  // Import typefaces
-  import 'typeface-montserrat'
-  import 'typeface-merriweather'
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+import './Divider.css'
 
-  import profilePic from './profile-pic.jpg'
-  import './Divider.css'
-  import { rhythm } from '../utils/typography'
-
-  class Divider extends React.Component {
-    render() {
-      return (
-
-      <hr/>
-
-
-      )
-    }
+class Divider extends React.Component {
+  render() {
+    return <hr />
   }
+}
 
-  export default Divider
+export default Divider
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,17 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import './Footer.css'
-
-import profilePic from './profile-pic.jpg'
-import { rhythm } from '../utils/typography'
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 class Footer extends React.Component {
   render() {
     return (
-
-    <p>
-          Made with <i className="fa fa-heart" aria-hidden="true"></i> &amp; <i className="fa fa-code" aria-hidden="true"></i>
-          &nbsp; in Brooklyn, New York by <a href="http://aboutmonica.com">Monica Powell</a>.
-    </p>
+      <p>
+        Made with <i className="fa fa-heart" aria-hidden="true" /> &amp;{' '}
+        <i className="fa fa-code" aria-hidden="true" />
+        &nbsp; in Brooklyn, New York by{' '}
+        <a href="http://aboutmonica.com">Monica Powell</a>.
+      </p>
     )
   }
 }
